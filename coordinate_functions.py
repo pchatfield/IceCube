159e509ddf0a508b37041740dc513694f424b1eb
Patrick Chatfield <pchatfield925@gmail.com>
2023-03-24 18:04:45 -0400

    Add spherical function

--- coordinate_functions.py
+++ coordinate_functions.py
@@ -6,3 +6,7 @@ def cartesian(azimuth, zenith):
     z = math.cos(zenith)
 
     return x, y, z
+
+def spherical(x,y,z):
+    zenith = math.acos(z/math.sqrt(x**2 + y**2 + z**2))
+    azimuth = math.atan2(y,x)
